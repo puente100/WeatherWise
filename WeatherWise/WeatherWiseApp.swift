321635bc76a341934d679839f3ff56611fa8715c
DanielPuente <puente10@hotmail.com>
2024-07-17 13:15:37 -0600

    Initial Commit

---
+++ WeatherWise/WeatherWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWiseApp.swift
+//  WeatherWise
+//
+//  Created by Daniel Puente on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
